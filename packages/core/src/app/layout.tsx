4e466885798c0c6643122a36e330a6354468db1c
Luca Schiavoi <jerv.norsk@gmail.com>
2025-12-04 02:21:07 +0100

    #2 - Create a page in under construction
    - Set favicon from logo-eilertenstudio.svg

--- packages/core/src/app/layout.tsx
+++ packages/core/src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout(
     <head>
       <meta charSet="UTF-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      {/*<meta http-equiv="refresh" content="2; URL='https://eilertenstudio.itch.io/'" />*/}
       <title>Eilerten Studio</title>
     </head>
     <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
